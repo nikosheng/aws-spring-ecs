5b4f56bd9b93a1d4d7355be6b7fa66917070e9bf
Niko <niko.js.feng@gmail.com>
2020-06-19 14:34:31 +0800

    New module - User service

--- ecs-order/src/main/java/ecs/order/controller/OrderController.java
+++ ecs-order/src/main/java/ecs/order/controller/OrderController.java
@@ -5,7 +5,6 @@ import ecs.order.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
@@ -15,9 +14,6 @@ import java.util.List;
 public class OrderController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
-
-    @Value("${variables.shipment.uri:http://localhost:8001}")
-    private String shipmentHost;
 
     @Autowired
     private OrderService orderService;
